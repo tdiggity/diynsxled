de737b559cf2984168922fb9127373a8fec9ee4d
Tim Nguyen <tnguyen@palantir.com>
2018-12-26 22:54:33 -0800

    first pass at local variables

--- diynsxled.ino
+++ diynsxled.ino
@@ -1,115 +1,155 @@
 /*
- * LED Tail lights for NSX
- * Uses eight 8x8 neopixel matrixes
- * Code is only for one side at the moment.
- */
+   LED Tail lights for NSX
+   All rights reserved
+   Intended for off-road use only
+*/
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
+#include <MemoryFree.h>
 #ifndef PSTR
-  #define PSTR // Make Arduino Due happy
+#define PSTR // Make Arduino Due happy
 #endif
 
 // Data output pins
 #define PASSENGER_MATRIX_PIN  5   // passenger side
+#define DRIVER_MATRIX_PIN     6   // passenger side
 
 // Opto-isolator inputs
 #define OPTO_IN_PARKING       7   // parking
 #define OPTO_IN_BRAKES        8  // brakes
 #define OPTO_IN_REVERSE       9  // reverse
 #define OPTO_IN_PASSENGER_TS  10  // passenger turn signal
+// driver side TS == 11
 
-// Color definitions - only used for black because you can't set 
-// brightness on these without changing them all. 
+// Color definitions - only used for black because you can't set
+// brightness on these without changing them all.
 #define BLACK    0x0000
 #define RED      0xF800
 #define ORANGE   0xFC60
 #define WHITE    0xFFFA
 #define BLUE     0x001F
 
-// default state
+// brightness_levels
+#define BRAKE_BRIGHTNESS    220
+#define PARKING_BRIGHTNESS  125
+#define TS_BRIGHTNESS       220
+#define REVERSE_BRIGHTNESS  125
+
+// default state of optocoupler inputs
 int pinState_passenger_ts = 0;
 int pinState_reverse = 0;
 int pinState_parking = 0;
 int pinState_brake = 0;
 
-// Matrix objects
-Adafruit_NeoMatrix passenger_matrix = Adafruit_NeoMatrix(
-  8, 8, // pixel width/height per matrix
-  8,    // number of tiles
-  1,    // width of tiles
-  PASSENGER_MATRIX_PIN,
-  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-  NEO_MATRIX_ROWS    + NEO_MATRIX_PROGRESSIVE,
-  NEO_GRB            + NEO_KHZ800);
+// for determining what leds to turn on
+boolean brakesOn = false;
+boolean parkingOn = false;
+boolean turnsignalOn = false;
+boolean reverseOn = false;
 
 void setup() {
   pinMode(OPTO_IN_PARKING,      INPUT_PULLUP);
   pinMode(OPTO_IN_BRAKES,       INPUT_PULLUP);
   pinMode(OPTO_IN_REVERSE,      INPUT_PULLUP);
   pinMode(OPTO_IN_PASSENGER_TS, INPUT_PULLUP);
-  passenger_matrix.begin();
-  passenger_matrix.show();
+  //Serial.begin(115200);
+  Serial.println("starting");
 }
 
 void loop() {
+  // get state of what lights should be on
   pinState_parking      = digitalRead(OPTO_IN_PARKING);
   pinState_brake        = digitalRead(OPTO_IN_BRAKES);
   pinState_reverse      = digitalRead(OPTO_IN_REVERSE);
   pinState_passenger_ts = digitalRead(OPTO_IN_PASSENGER_TS);
   delay(1); // delay in between reads for stability
 
-  // parking and brake lights
+  Serial.print("freeMemory()=");
+  Serial.println(freeMemory());
+
+  // analyze the state of lights
   // brake lights take priority over parking lights.
   if (pinState_brake == 1) {
-    setBrakeMatrix(240);
+    brakesOn = true;
+    parkingOn = false;
   } else if (pinState_parking == 1) {
-    setBrakeMatrix(150);
+    parkingOn = true;
+    brakesOn = false;
   } else {
-    resetBrakes();
+    brakesOn = false;
+    parkingOn = false;
   }
 
   //reverse lights
   if (pinState_reverse == 1) {
-    setReverse(150);
+    reverseOn = true;
   } else {
-    resetReverse();
+    reverseOn = false;
   }
 
   //left turnsignal
   if (pinState_passenger_ts == 1) {
-    setTurnSignal(200);
+    turnsignalOn = true;
   } else {
-    resetTurnSignal();
+    turnsignalOn = false;
   }
 
-  passenger_matrix.show();
+  // we do it this way because there isn't enough memory if you make the neopixels global.
+  setLeds(brakesOn, parkingOn, reverseOn, turnsignalOn, PASSENGER_MATRIX_PIN);
+  setLeds(brakesOn, parkingOn, reverseOn, turnsignalOn, DRIVER_MATRIX_PIN);
 }
 
-void setBrakeMatrix(int brightness) {
-  passenger_matrix.fillRoundRect(8,  0, 12, 8, 1, passenger_matrix.Color(brightness, 0, 0));
-  passenger_matrix.fillRoundRect(22, 0, 12, 8, 1, passenger_matrix.Color(brightness, 0, 0));
-  passenger_matrix.fillRoundRect(36, 0, 12, 8, 1, passenger_matrix.Color(brightness, 0, 0));
-}
+void setLeds(boolean brakesOn, boolean parkingOn, boolean reverseOn, boolean turnsignalOn, 
+              int pin) {
 
-void resetBrakes() {
-  passenger_matrix.fillRect(8, 0, 40, 8, BLACK);
-}
+  unsigned long StartTime = millis();
+  // Matrix objects
+  Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(
+                                8, 8, // pixel width/height per matrix
+                                7,    // number of tiles
+                                1,    // width of tiles
+                                pin,
+                                NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+                                NEO_MATRIX_ROWS    + NEO_MATRIX_PROGRESSIVE,
+                                NEO_GRB            + NEO_KHZ800);
+  matrix.begin(); // init matrix
+  
+  if (brakesOn) {
+    // only show 2 beause that's how stock setup does it
+    //matrix.fillRoundRect(8,  0, 12, 8, 1, matrix.Color(BRAKE_BRIGHTNESS, 0, 0));
+    matrix.fillRoundRect(22, 0, 12, 8, 1, matrix.Color(BRAKE_BRIGHTNESS, 0, 0));
+    matrix.fillRoundRect(36, 0, 12, 8, 1,matrix.Color(BRAKE_BRIGHTNESS, 0, 0));
+  } else if(parkingOn) {
+    matrix.fillRoundRect(8,  0, 12, 8, 1, matrix.Color(PARKING_BRIGHTNESS, 0, 0));
+    matrix.fillRoundRect(22, 0, 12, 8, 1, matrix.Color(PARKING_BRIGHTNESS, 0, 0));
+    matrix.fillRoundRect(36, 0, 12, 8, 1, matrix.Color(PARKING_BRIGHTNESS, 0, 0));
+  }
 
-void setReverse(int brightness) {
-  passenger_matrix.fillRect(0, 0, 8, 8, passenger_matrix.Color(brightness, brightness, brightness));
-}
+  if (reverseOn) {
+    matrix.fillRect(0, 0, 8, 8, matrix.Color(REVERSE_BRIGHTNESS, REVERSE_BRIGHTNESS, REVERSE_BRIGHTNESS));
+  }
 
-void resetReverse() {
-  passenger_matrix.fillRect(0, 0, 8, 8, BLACK);
-}
+  if (turnsignalOn) {
+    matrix.fillRoundRect(48, 0, 8, 8, 1, matrix.Color(TS_BRIGHTNESS, 0, 0));
+  }
 
-void setTurnSignal(int brightness) {
-  passenger_matrix.fillRect(48, 0, 5, 8, passenger_matrix.Color(brightness, 0, 0));
-  passenger_matrix.fillRect(56, 0, 8, 8, passenger_matrix.Color(brightness, 0, 0));
-}
-
-void resetTurnSignal() {
-  passenger_matrix.fillRect(48, 0, 16, 8, BLACK);
+  matrix.show();
+  Serial.print("P ");
+  Serial.print(parkingOn);
+  Serial.print(" B ");
+  Serial.print(brakesOn);
+  Serial.print(" R ");
+  Serial.print(reverseOn);
+  Serial.print(" TS ");
+  Serial.print(turnsignalOn);
+  Serial.print(" PIN ");
+  Serial.print(pin);
+  Serial.print(" Matrix: freeMemory()=");
+  Serial.print(freeMemory());
+  unsigned long CurrentTime = millis();
+  unsigned long ElapsedTime = CurrentTime - StartTime;
+  Serial.print("\t Time to calculate: ");
+  Serial.println(ElapsedTime);
 }
